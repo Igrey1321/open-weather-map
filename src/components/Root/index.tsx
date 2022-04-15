16f9e9ad2a07016abd9faebb34097620e1688c6d
Ihor Radvanskyi <igrey1321@gmail.com>
2022-04-15 15:39:29 +0300

    remove not found

--- src/components/Root/index.tsx
+++ src/components/Root/index.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from '../Layout';
 import React from 'react';
-import NotFoundScreen from '../../pages/NotFoundScreen';
 import Home from '../../pages/home';
 import Detail from '../../pages/detail/Detail';
 
@@ -9,9 +8,8 @@ export default function Root() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
         <Route path="/city/:name" element={<Detail />} />
-        <Route path="*" element={<NotFoundScreen />} />
       </Routes>
     </Layout>
   );
